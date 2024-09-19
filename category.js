89f86913c96869028c0f73258c32b56c6f9f53ac
Keerthi Reddy Gudibandi <161864492+KeerthiReddyGudibandi@users.noreply.github.com>
2024-09-19 13:52:18 +0530

    Add files via upload

--- category.js
+++ category.js
@@ -1,59 +1,14 @@
-const CategorySchema = require("../models/CategorySchema");
-const slugify = require('slugify');
-const{ errorHandler } = require('../helpers/dbErrorHandler');
+const exppress = require('express');
+const {requireSignIn, adminMiddleware } = require('../controllers/auth');
+const { create, list, read, remove } = require('../controllers/category');
+const { runValidation} = require('../validations');
+const { categoryCreateValidator } = require('../validations/category');
+const router = exppress.Router();
 
-//Create Category
-exports.create = (req, res) => {
-    const {name} = req.body;
-    let slug = slugify(name).toLowerCase();
+router.post('/category', categoryCreateValidator, runValidation, requireSignIn, adminMiddleware, create)
+    .get('/categories', list)
+    .get('/category/:slug', read)
+    .delete('/category/:slug', requireSignIn, adminMiddleware, remove);
 
-    let category = new CategorySchema({name, slug});
-    category.save((err, data) => {
-        if(err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
-        res.json(data);
-    })
-}
 
-//List Category
-exports.list = (req, res) => {
-    CategorySchema.find({}, (err, data) => {
-        if(err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
-        res.json(data);
-    }).select('name slug');
-}
-
-//Read Category
-exports.read = (req, res) => {
-    const slug = req.params.slug.toLowerCase();
-    CategorySchema.findOne({slug}, (err, data) => {
-        if(err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
-        res.json(data);
-    }).select('name slug');
-}   
-
-//Remove Category
-exports.remove = (req, res) => {
-    const slug = req.params.slug.toLowerCase();
-    CategorySchema.findOneAndRemove({slug}, (err, data) => {
-        if(err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
-        res.json({
-            message: 'Category deleted successfully'
-        });
-    }).select('name slug');
-}
+module.exports = router;
